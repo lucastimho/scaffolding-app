63714609ed98d11f06fd6a23617644cedc1c828a
lucastimho <lucas.t.ho.lh@gmail.com>
2021-11-07 23:30:13 -0600

    adds scaffolding steps 5 - 7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,45 +4,82 @@ class PostsController < ApplicationController
   # GET /posts
   def index
     @posts = Post.all
+
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render :json => @posts }
+    end
   end
 
   # GET /posts/1
   def show
+    @post = Post.find(params[:id])
+ 
+    respond_to do |format|
+      format.html  # show.html.erb
+      format.json  { render :json => @post }
+    end
   end
 
   # GET /posts/new
   def new
     @post = Post.new
+
+    respond_to do |format|
+      format.html  # new.html.erb
+      format.json  { render :json => @post }
+    end
   end
 
   # GET /posts/1/edit
   def edit
+    @post = Post.find(params[:id])
   end
 
   # POST /posts
   def create
     @post = Post.new(post_params)
 
-    if @post.save
-      redirect_to @post, notice: 'Post was successfully created.'
-    else
-      render :new
+    respond_to do |format|
+      if @post.save
+        format.html  { redirect_to(@post,
+                      :notice => 'Post was successfully created.') }
+        format.json  { render :json => @post,
+                      :status => :created, :location => @post }
+      else
+        format.html  { render :action => "new" }
+        format.json  { render :json => @post.errors,
+                      :status => :unprocessable_entity }
+      end
     end
   end
 
   # PATCH/PUT /posts/1
   def update
-    if @post.update(post_params)
-      redirect_to @post, notice: 'Post was successfully updated.'
-    else
-      render :edit
+    @post = Post.find(params[:id])
+ 
+    respond_to do |format|
+      if @post.update_attributes(params[:post])
+        format.html  { redirect_to(@post,
+                      :notice => 'Post was successfully updated.') }
+        format.json  { head :no_content }
+      else
+        format.html  { render :action => "edit" }
+        format.json  { render :json => @post.errors,
+                      :status => :unprocessable_entity }
+      end
     end
   end
 
   # DELETE /posts/1
   def destroy
-    @post.destroy
-    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+    @post = Post.find(params[:id])
+  @post.destroy
+ 
+    respond_to do |format|
+      format.html { redirect_to posts_url }
+      format.json { head :no_content }
+    end
   end
 
   private
